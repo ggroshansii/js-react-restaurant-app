655b5735f0ab5d0170ed1d72832ca3dacb33cd73
ggroshansii <garthgroshansii@gmail.com>
2021-10-02 12:18:57 -0400

    completed nav abar styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,10 +9,10 @@ function NavBar(props) {
   return (
     <div>
       <div className="nav-container">
-        <button value='home' name='home' onClick={() => props.changeNavSelection('home')}>Home</button>
-        <button value="menu" name='menu' onClick={() => props.changeNavSelection('menu')}>Menu</button>
-        <button value='aboutus' name='aboutus' onClick={() => props.changeNavSelection('aboutus')}>About Us</button>
-        <button value="contactus" name='contactus' onClick={() => props.changeNavSelection('contactus')}>Contact Us</button>
+        <button value='home' name='home' onClick={() => props.changeNavSelection('home')}>HOME</button>
+        <button value="menu" name='menu' onClick={() => props.changeNavSelection('menu')}>MENU</button>
+        <button value='aboutus' name='aboutus' onClick={() => props.changeNavSelection('aboutus')}>ABOUT US</button>
+        <button value="contactus" name='contactus' onClick={() => props.changeNavSelection('contactus')}>CONTACT US</button>
         {/* <button value="aboutus" name='aboutus'>About Us</button>
         <button value="contact" name='contact'>Contact</button> */}
 
