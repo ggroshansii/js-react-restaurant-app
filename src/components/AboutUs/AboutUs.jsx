95102c2e78736314efbb9dcd65438c35f4a5af61
ggroshansii <garthgroshansii@gmail.com>
2021-10-02 17:26:35 -0400

    finished styling the 'About Us' page

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,9 +1,20 @@
-import "./AboutUs.css"
+import "./AboutUs.css";
 
 function AboutUs() {
     return (
-        <div>null</div>
-    )
+        <div className="aboutus-container">
+            <div className="aboutus-blurb">
+                <h2>About Ya Mama's:</h2>
+                <p>
+                    Ya Mama's Pizza prides themselves in their unique
+                    fire-inspired New York style pizzas and dope
+                    craft beers. With fresh ingredients imported directly from
+                    Italy, each pizza is formed by hand and cooked in a wood fired oven! One bite and
+                    one sip, you’ll enjoy nothing but the best from Ya Mama!
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default AboutUs;
